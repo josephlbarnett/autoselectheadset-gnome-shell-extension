e47e0df2a08dd841590e755fabbd4cc087758f42
Joe Barnett <jbarnett@tribedynamics.com>
2020-12-11 11:55:11 -0800

    Initial commit of autoselect headset extension

---
+++ extension.js
@@ -0,0 +1,65 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const AudioDeviceSelectionDBus = imports.ui.audioDeviceSelection.AudioDeviceSelectionDBus;
+const AudioDevice = imports.ui.audioDeviceSelection.AudioDevice;
+const Main = imports.ui.main;
+const { GLib } = imports.gi;
+
+const AutoSelectHeadsetAsync = function(params, invocation) {
+    let [deviceNames] = params;
+    let deviceName = 'headset';
+    if (deviceNames.indexOf('headset') < 0) {
+        deviceName = 'headphones';
+    }
+    if (deviceNames.indexOf('headphones') < 0) {
+        deviceName = 'microphone';
+    }
+    if (deviceNames.indexOf('microphone') < 0) {
+        invocation.return_value(null);
+        return;
+    }
+    let connection = Main.shellAudioSelectionDBusService._dbusImpl.get_connection();
+    let info = Main.shellAudioSelectionDBusService._dbusImpl.get_info();
+    connection.emit_signal(invocation.get_sender(),
+                           Main.shellAudioSelectionDBusService._dbusImpl.get_object_path(),
+                           info ? info.name : null,
+                           'DeviceSelected',
+                           GLib.Variant.new('(s)', [deviceName]));
+    invocation.return_value(null);
+}
+
+const originalOpenAsync = Main.shellAudioSelectionDBusService.OpenAsync;
+class AutoSelectHeadsetExtension {
+    constructor() {
+    }
+
+    enable() {
+        Main.shellAudioSelectionDBusService.OpenAsync = AutoSelectHeadsetAsync
+    }
+
+    disable() {
+        Main.shellAudioSelectionDBusService.OpenAsync = originalOpenAsync;
+    }
+}
+
+function init() {
+    return new AutoSelectHeadsetExtension();
+}
